a231c3e98f26ba5b441da82356cffdbd58ea5598
neuroarmor <ekaterine.nebieridze.1@iliauni.edu.ge>
2025-03-22 04:25:58 +0400

    natura

---
+++ script/bulb.js
@@ -0,0 +1,12 @@
+let address=document.getElementById("nat").src;
+console.log(address);
+
+let onoff = () => {
+    let nat = document.getElementById("nat");
+    if (nat.src == "file:///C:/Users/User/Desktop/pirveli%20js/img/off.png") {
+        nat.src = "img/on.png";  
+    } 
+    else {
+        nat.src = "img/off.png";  
+    }
+}
