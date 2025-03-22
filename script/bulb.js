f8e44bc6bb908ec5e85834cfaa2e5ba1ca0fe09d
neuroarmor <ekaterine.nebieridze.1@iliauni.edu.ge>
2025-03-22 05:09:58 +0400

    Shecvlilinatura

--- script/bulb.js
+++ script/bulb.js
@@ -1,12 +1,22 @@
 let address=document.getElementById("nat").src;
 console.log(address);
 
+// let onoff = () => {
+//     let nat = document.getElementById("nat");
+//     if (nat.src == "file:///C:/Users/User/Desktop/pirveli%20js/img/off.png") {
+//         nat.src = "img/on.png";  
+//     } 
+//     else {
+//         nat.src = "img/off.png";  
+//     }
+// }
+// არ იმუშავა გითჰაბში
 let onoff = () => {
     let nat = document.getElementById("nat");
-    if (nat.src == "file:///C:/Users/User/Desktop/pirveli%20js/img/off.png") {
-        nat.src = "img/on.png";  
-    } 
+    if (nat.src.includes("off.png")) { 
+        nat.src = "img/on.png";
+    }
     else {
-        nat.src = "img/off.png";  
+        nat.src = "img/off.png";
     }
 }
